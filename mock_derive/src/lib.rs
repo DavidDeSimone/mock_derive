7b0256a38d7ffe1be10233faba9e9ed6a16069c6
DavidDeSimone <djd231@scarletmail.rutgers.edu>
2017-07-06 20:16:18 -0700

    Adding MIT header to files. Fleshing out basic mockable macro use

--- mock_derive/src/lib.rs
+++ mock_derive/src/lib.rs
@@ -1,3 +1,27 @@
+/*
+MIT License
+
+Copyright (c) 2017 David DeSimone
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
 #![feature(proc_macro)]
 #![recursion_limit = "128"]
 
@@ -13,4 +37,20 @@ use std::str::FromStr;
 #[proc_macro_attribute]
 pub fn mock(attr_ts: TokenStream, fn_ts: TokenStream) -> TokenStream {
     attr_ts
+}
+
+#[proc_macro_attribute]
+pub fn mockable(attr_ts: TokenStream, fn_ts: TokenStream) -> TokenStream {
+    let fn_item = syn::parse_item(&fn_ts.to_string()).unwrap();
+    let ident = &fn_item.ident;
+    let stream = quote! {
+        #fn_item
+        
+        impl #ident {
+            pub fn new_mock() {
+            }
+        }
+    };
+
+    TokenStream::from_str(stream.as_str()).unwrap()
 }
