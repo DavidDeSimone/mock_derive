abfe8b0aad3da1f8687a8031a1530c9a8d842ccf
DavidDeSimone <djd231@scarletmail.rutgers.edu>
2017-07-11 23:00:33 -0700

    Commiting a horrible, WIP, warning filled attempt at getting result mocking actually working

--- mock_derive/src/lib.rs
+++ mock_derive/src/lib.rs
@@ -36,6 +36,7 @@ use std::str::FromStr;
 
 struct Function {
     pub name: syn::Ident,
+    pub decl: syn::FnDecl
 }
 
 fn parse_impl(item: &syn::Item) -> Vec<Function> {
@@ -43,7 +44,12 @@ fn parse_impl(item: &syn::Item) -> Vec<Function> {
     match item.node {
         syn::ItemKind::Impl(unsafety, impl_token, ref generics, ref trait_, ref self_ty, ref items) => {
             for item in items {
-                result.push(Function {name: item.ident.clone() });
+                match item.node {
+                    syn::ImplItemKind::Method(ref sig, ref block) => {
+                        result.push(Function {name: item.ident.clone(), decl: sig.decl.clone() } );
+                    },
+                    _ => { }
+                }
             }
         },
         _ => { panic!("#[mock] must be applied to an Impl statement."); }
@@ -63,27 +69,48 @@ pub fn mock(attr_ts: TokenStream, impl_ts: TokenStream) -> TokenStream {
     // object to mutate
     for fnc in fns {
         let name = fnc.name;
+        let decl = fnc.decl.inputs;
         let name_stream = quote! { #name };
         let ident = concat_idents("method_", name_stream.as_str());
-        methods = quote! {
-            #methods
-            pub fn #ident(&mut self) -> MockMethod<T> {
-                MockMethod { imp: self }
+        // @TODO make MockMethod also take in a U, where U will be the type of tuple
+        // based on the types in 'inputs'
+        let mut args = Vec::new();
+        for input in decl {
+            match input {
+                syn::FnArg::Captured(pat, ty) => {
+                    args.push(ty.clone());
+                },
+                _ => {}
             }
         }
-    }
+
+        let tuple_type = match args.len() {
+            0 => { quote!{ () } },
+            1 => { quote!{ (#args[0]) } },
+            2 => { quote!{ (#args[0], #args[1]) } },
+            3 => { quote!{ (#args[0], #args[1], #args[2]) } },
+            _ => { panic!("Unexpected number of args, max is 3") }
+        };
+        
+        methods = quote! {
+            #methods
+            pub fn #ident(&mut self) -> MockMethod<T, #tuple_type> {
+                MockMethod { imp: self, retval: Vec::new() }
+            }
+        }
+    }    
     
     let stream = quote! {
         // @TODO make unique name
         struct MockImpl<T> {
             fallback: T,
-            call_num: i32,
-            // @TODO hashmap of callchains
+            call_num: usize,
         }
 
         // @TODO add impl block that adds mock functionality
-        struct MockMethod<'a, T: 'a> {
+        struct MockMethod<'a, T: 'a, U> {
             pub imp: &'a mut MockImpl<T>,
+            retval: Vec<U>,
         }
 
         impl<T> MockImpl<T> {
@@ -94,7 +121,7 @@ pub fn mock(attr_ts: TokenStream, impl_ts: TokenStream) -> TokenStream {
             }
         }
 
-        impl<'a, T: 'a> MockMethod<'a, T> {
+        impl<'a, T: 'a, U> MockMethod<'a, T, U> {
             pub fn first_call(mut self) -> Self {
                 self.nth_call(1)
             }
@@ -103,14 +130,16 @@ pub fn mock(attr_ts: TokenStream, impl_ts: TokenStream) -> TokenStream {
                 self.nth_call(2)
             }
 
-            pub fn nth_call(mut self, num: i32) -> Self {
+            pub fn nth_call(mut self, num: usize) -> Self {
+                self.retval.reserve(num);
                 self.imp.call_num = num;
                 self
             }
 
             // @TODO U in this case will be a tuple of results, that will be unpacked
             // and applied to a function. We need to figure out how to store this tuple
-            pub fn set_result<U>(self, tuple: U) -> Self {
+            pub fn set_result(mut self, tuple: U) -> Self {
+                self.retval[self.imp.call_num] = tuple;
                 self
             }
         }
